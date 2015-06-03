a6448b8b403b9bea90b8d671aaebec0ad835f714
Aaron Sung <aaronsung@gmail.com>
2015-06-03 23:02:07 +0800

    Added normal user login

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,17 @@
 Rails.application.routes.draw do
+ 
+  controller :sessions do
+    get 'login' => :new
+    post 'login' => :create
+    delete 'logout' => :destroy
+  end
+
+  # get 'sessions/new'
+  
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
   resources :articles
 
   resources :editors
