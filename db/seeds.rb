892028dfe6ce0dbc6ec7bc9732cc95bf3ff279e7
Aaron Sung <aaronsung@gmail.com>
2015-06-07 22:04:59 +0800

    Added admin to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Editor.create(name: 'admin',email:'admin', password: 'admin',is_admin: true)
