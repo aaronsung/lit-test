2e703dfc49eb04b1bdfeb5ba382369f83a1c9aff
Aaron Sung <aaronsung@gmail.com>
2015-06-24 22:51:48 +0800

    Update delete article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -79,7 +79,7 @@ class ArticlesController < ApplicationController
   def destroy
     @article.destroy
     respond_to do |format|
-      format.html { redirect_to articles_url }
+      format.html { redirect_to action:'catalog' }
       #, notice: 'Article was successfully destroyed.'
       format.json { head :no_content }
     end
