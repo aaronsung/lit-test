b32d917c8794996a49d781657977b7d3a39659a3
Aaron Sung <aaronsung@gmail.com>
2015-06-29 00:03:42 +0800

    Basic comments function

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150623152645) do
+ActiveRecord::Schema.define(version: 20150628153438) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -25,6 +25,16 @@ ActiveRecord::Schema.define(version: 20150623152645) do
   end
 
   add_index "articles", ["editor_id"], name: "index_articles_on_editor_id"
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "editor"
+    t.text     "body"
+    t.integer  "article_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "comments", ["article_id"], name: "index_comments_on_article_id"
 
   create_table "editors", force: :cascade do |t|
     t.string   "name"
