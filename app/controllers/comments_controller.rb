35292fe64630bce37b9e33c651bcabca847c3d0a
Aaron Sung@Could 9 <aaron.sung@gmail.com>
2015-07-01 03:58:44 +0000

    Finish Comment function

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,12 +1,14 @@
 class CommentsController < ApplicationController
     def create
     @article = Article.find(params[:article_id])
-    @comment = @article.comments.create(comment_params)
+    @comment = @article.comments.new(comment_params)
+    @comment.editor = session[:user_name]
+    @comment.save
     redirect_to article_path(@article)
   end
  
   private
     def comment_params
-      params.require(:comment).permit(:commenter, :body)
+      params.require(:comment).permit(:editor, :body)
     end
 end
